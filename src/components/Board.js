d1ae6f8030cbad9c3c90abb667da1921fd24ddb7
rnogara <r.nogara.dev@gmail.com>
2023-02-19 13:08:35 -0300

    add 5 random colours to pallete

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/Board.css';
 
 export default class Board extends Component {
+  state = {
+    boardSize: 5,
+    boardBoxWidth: 200,
+    errorMessage: '',
+  }
+
+  handleBoardSize = ({ target: { value } }) => {
+    if ( value < 5) {
+      this.setState({
+        errorMessage: 'Invalid Board Size, it must be higher than 5px',
+      })
+    } else {
+      this.setState({
+        boardSize: value,
+      })
+    }
+  }
+
   render() {
+    const { boardSize } = this.state;
     return (
-      <div>Board</div>
+      <section style={{}}>
+        <label htmlFor='board-size'>
+          <input
+            id='board-size'
+            type='number'
+            onChange={ this.handleBoardSize }
+            value={ boardSize }
+          />
+          x Board Size
+        </label>
+
+      </section>
     )
   }
 }
--- src/components/Pallete.js
+++ src/components/Pallete.js
@@ -1,9 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/Pallete.css';
 
 export default class Pallete extends Component {
+  state = {
+    palleteColors: ['rgb(0, 0, 0)'],
+    randomPickedColor: '',
+  }
+
+  componentDidMount() {
+    this.randomColor();
+  }
+
+  componentDidUpdate() {
+    const { palleteColors } = this.state;
+    if (palleteColors.length < 5) {
+      this.randomPalleteColor();
+    }
+  }
+
+  randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    this.setState({
+      randomPickedColor: `rgb(${r}, ${g}, ${b})`,
+    });
+  }
+
+  randomPalleteColor = () => {
+    const { palleteColors, randomPickedColor } = this.state
+    const colorPallete = JSON.parse(localStorage.getItem('colorPallete'));
+    if (colorPallete) {
+      this.setState({
+        palleteColors: colorPallete,
+      });
+      return;
+    }
+    const colorCheck = palleteColors.some((color) => color === randomPickedColor);
+    if (!colorCheck) {
+      this.setState((prevState) => ({
+        palleteColors: [...prevState.palleteColors, randomPickedColor],
+      }), () => {
+        const { palleteColors } = this.state;
+        if (palleteColors.length === 5) {
+          localStorage.setItem('colorPallete', JSON.stringify(palleteColors));
+        }
+      });
+    }
+    this.randomColor();
+  }
+
   render() {
+    const { palleteColors } = this.state;
     return (
-      <div>Pallete</div>
+      <section className='pallete'>
+        {
+          palleteColors.map((color, index) => (
+            <div
+              key={ index }
+              style={{backgroundColor: color}}
+              // onClick={ this.handlePalleteClick }
+            >
+            </div>)
+          )
+        }
+      </section>
     )
   }
 }
